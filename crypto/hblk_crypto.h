afb2bc0fa255455baed16f42b328bbcd227407ca
guxal <jonathanacp93@gmail.com>
2021-12-12 23:38:30 +0000

    feat: add save Key

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,12 +1,23 @@
 #ifndef HBLK_CRYPTO
 #define HBLK_CRYPTO
 
-#include <openssl/sha.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#include <openssl/sha.h>
 #include <openssl/ec.h>
 #include <openssl/evp.h>
+/* #include <openssl/bn.h> */
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <openssl/pem.h>
 
 #define EC_CURVE   NID_secp256k1
 
@@ -40,5 +51,6 @@ uint8_t *sha256(int8_t const *s, size_t len,
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
 
 #endif

03d250bdec17ecc3837bdd8a3ba9de97d81d0888
guxal <jonathanacp93@gmail.com>
2022-01-06 06:03:26 +0000

    feat: blockchain destroy func

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -108,4 +108,5 @@ blockchain_t *blockchain_create(void);
 block_t *block_create(block_t const *prev, int8_t const *data,
 	uint32_t data_len);
 void block_destroy(block_t *block);
+void blockchain_destroy(blockchain_t *blockchain);
 #endif /* __BLOCKCHAIN_H_ */

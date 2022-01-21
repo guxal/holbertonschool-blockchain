9145b79d47e571160b605ff064374a1439637064
guxal <jonathanacp93@gmail.com>
2022-01-21 00:15:44 +0000

    feat: add v0.3 - transaction - tx out create

---
+++ blockchain/v0.3/transaction/transaction.h
@@ -0,0 +1,23 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include "blockchain.h"
+
+/**
+ * struct tx_out_s - Transaction output
+ *
+ * @amount: Amount received
+ * @pub:	Receiver's public address
+ * @hash:	Hash of @amount and @pub. Serves as output ID
+ */
+typedef struct tx_out_s
+{
+	uint32_t	amount;
+	uint8_t		pub[EC_PUB_LEN];
+	uint8_t		hash[SHA256_DIGEST_LENGTH];
+} tx_out_t;
+
+tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
+
+
+#endif

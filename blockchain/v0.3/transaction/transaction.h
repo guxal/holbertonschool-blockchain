0fe817ed15a4a4ce70b12816398915c9067e950d
guxal <jonathanacp93@gmail.com>
2022-01-21 15:45:13 +0000

    feat: add tx in create

--- blockchain/v0.3/transaction/transaction.h
+++ blockchain/v0.3/transaction/transaction.h
@@ -35,6 +35,28 @@ typedef struct unspent_tx_out_s
 	tx_out_t    out;
 } unspent_tx_out_t;
 
+/**
+ * struct tx_in_s - Transaction input
+ *
+ * Description: A transaction input always refers to a previous
+ * transaction output. The only exception is for a Coinbase transaction, that
+ * adds new coins to ciruclation.
+ *
+ * @block_hash:  Hash of the Block containing the transaction @tx_id
+ * @tx_id:       ID of the transaction containing @tx_out_hash
+ * @tx_out_hash: Hash of the referenced transaction output
+ * @sig:         Signature. Prevents anyone from altering the content of the
+ *               transaction. The transaction input is signed by the receiver
+ *               of the referenced transaction output, using their private key
+ */
+typedef struct tx_in_s
+{
+	uint8_t     block_hash[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_id[SHA256_DIGEST_LENGTH];
+	uint8_t     tx_out_hash[SHA256_DIGEST_LENGTH];
+	sig_t       sig;
+} tx_in_t;
+
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN]);
 
 unspent_tx_out_t
@@ -42,4 +64,5 @@ unspent_tx_out_t
 										uint8_t tx_id[SHA256_DIGEST_LENGTH],
 										tx_out_t const *out);
 
+tx_in_t *tx_in_create(unspent_tx_out_t const *unspent);
 #endif
